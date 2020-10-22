6346996e9dc25c73f50dfdb9168fbb0c1e246420
Seth Caldwell <caldwellst@gmail.com>
2020-10-22 13:57:48 +0100

    Add function to convert indicator codes

--- R/data_access.R
+++ R/data_access.R
@@ -17,6 +17,27 @@ billion_ind_codes <- function(billion = c("hep", "hpop", "uhc")) {
   codes
 }
 
+#' Convert indicator codes between types
+#'
+#' `convert_ind_codes()` allows easy conversion of indicator codes from dashboard
+#' IDs, xMart IDs, and GHO codes.
+#'
+#' @param ind_codes Character vector of indicator codes
+#' @param from Type of code being passed to `convert_ind_codes()`.
+#' @param to Type of code to return.
+#'
+#' @return A character vector.
+#'
+#' @export
+convert_ind_codes <- function(ind_codes,
+                             from = c("dashboard_id", "analysis_code", "gho_code"),
+                             to = c("dashboard_id", "analysis_code", "gho_code")) {
+  from <- rlang::arg_match(from)
+  to <- rlang::arg_match(to)
+  df <- billionaiRe::indicator_df
+  df[[to]][match(ind_codes, df[[from]])]
+}
+
 #' Get SDI ratio data
 #'
 #' Matches country ISO3 codes to data in the `sdi_ratio` data frame.
@@ -27,3 +48,4 @@ billion_ind_codes <- function(billion = c("hep", "hpop", "uhc")) {
 get_sdi_ratio <- function(iso3) {
   sdi_ratio[['sdiratio']][match(iso3, sdi_ratio[['iso3']])]
 }
+
