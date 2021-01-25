6e146b5a92637787ea137619b2c3d89c682453b3
Seth Caldwell <caldwellst@gmail.com>
2021-01-25 11:28:27 +0000

    close #2, add sex to pop_links and HPOP double counting correction

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# billionaiRe 0.2.1
+
+* Fixed pop_links to ensure that IPV double counting correction was correct 
+
 # billionaiRe 0.2.0
 
 * Added a `NEWS.md` file to track changes to the package.
--- data-raw/pop_links.R
+++ data-raw/pop_links.R
@@ -2,8 +2,17 @@
 library(tidyverse)
 
 pop_links <- read_csv("data-raw/pop_links.csv") %>%
-  pivot_longer(-ind, names_to = "pop_group") %>%
-  filter(value == 1) %>%
-  select(-value)
+  mutate(female = ifelse(sex %in% c("female", "both"),
+                         1,
+                         0),
+         male = ifelse(sex %in% c("male", "both"),
+                       1,
+                       0)) %>%
+  select(-sex) %>%
+  pivot_longer(-c(ind,female,male), names_to = "pop_group") %>%
+  pivot_longer(c(female, male), names_to = "sex", values_to = "sex_value") %>%
+  filter(value == 1 & sex_value == 1) %>%
+  mutate(pop_group = paste(pop_group, sex, sep = "_")) %>%
+  select(-value, -sex_value, -sex)
 
 usethis::use_data(pop_links, overwrite = TRUE, internal = FALSE)
