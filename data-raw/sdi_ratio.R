84aafea732440ac9e311ed68d02ffe69b83a61b3
Seth Caldwell <caldwellst@gmail.com>
2021-02-25 11:36:40 +0000

    Update script for SDI ratio calculation

--- data-raw/sdi_ratio.R
+++ data-raw/sdi_ratio.R
@@ -9,11 +9,13 @@ sdi_df2 <- sdi_df %>%
                               fuzzy_matching = "user_input"))
 
 sdi_ratio <- sdi_df2 %>%
-  filter(is_who_member(iso3),
-         !(iso3 == "GEO" & `1990` == "0·742")) %>% # remove the US state of Georgia
+  filter(is_who_member(iso3)) %>% # remove the US state of Georgia
   transmute(iso3 = iso3,
             value = paste0("0.", str_match(`2018`, "0\\·([0-9]+)$")[,2]),
             value = as.numeric(value),
+            value = ifelse(iso3 %in% c("SOM", "NER"),
+                           value[iso3 == "TCD"],
+                           value),
             sdiratio = -35.69 + 497.40 * value - 1272.90 * (value^2) + 1075.56 * (value^3)) %>%
   select(iso3, sdiratio)
 
