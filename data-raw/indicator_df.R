a3c0a151c3facd88830b3d3ed244426bb4b61634
ElliottMess <36757002+ElliottMess@users.noreply.github.com>
2021-07-26 20:00:20 +0200

    reorder indicator_df

--- data-raw/indicator_df.R
+++ data-raw/indicator_df.R
@@ -9,7 +9,8 @@ indicator_order <- read_excel("data-raw/indicator_order.xlsx")
 indicator_df <- read_excel("data-raw/indicator_df.xlsx") %>%
   mutate(across(c(uhc, hpop, hep, covariate, calculated),
                 ~ifelse(is.na(.x), F, T))) %>%
-  left_join(indicator_order, by = c("analysis_code" = "ind"), keep = TRUE)
+  left_join(indicator_order, by = c("analysis_code" = "ind"), keep = TRUE) %>%
+  arrange(order)
 
 usethis::use_data(indicator_df, overwrite = TRUE, internal = FALSE)
 
