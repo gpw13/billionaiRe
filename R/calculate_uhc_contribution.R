ff6cecac9e4e9f76fbf357dbd4808b2f51ad2302
Seth Caldwell <caldwellst@gmail.com>
2021-07-26 08:45:42 +0100

    Calculate contribution_pct for UHC

--- R/calculate_uhc_contribution.R
+++ R/calculate_uhc_contribution.R
@@ -1,8 +1,9 @@
 #' Calculate UHC contribution
 #'
-#' `calculate_uhc_contribution()` calculates country-level UHC contributions based on
-#' indicator level data. Calculates it for each country-year combination in the provided data,
-#' and for specific scenarios if specified.
+#' `calculate_uhc_contribution()` calculates country-level UHC contributions, in
+#' total population and percents, based on indicator level data. Calculates it
+#' for each country-year combination in the provided data, and for specific
+#' scenarios if specified.
 #'
 #' @inherit calculate_uhc_billion return details params
 #' @inherit calculate_hpop_contributions params
@@ -18,24 +19,32 @@ calculate_uhc_contribution <- function(df,
                                        pop_year = 2023,
                                        transform_value = "transform_value",
                                        contribution = stringr::str_replace(transform_value, "transform_value", "contribution"),
+                                       contribution_pct = paste0(contribution, "_percent"),
                                        scenario = NULL,
                                        ind_ids = billion_ind_codes("uhc", include_calculated = TRUE)) {
   assert_columns(df, year, iso3, ind, scenario, transform_value)
   assert_same_length(transform_value, contribution)
+  assert_same_length(contribution, contribution_pct)
   assert_years(start_year, end_year)
   assert_unique_rows(df, ind, iso3, year, scenario, ind_ids)
 
-  df <- billionaiRe_add_columns(df, contribution, NA_real_) %>%
+  df <- billionaiRe_add_columns(df, c(contribution, contribution_pct), NA_real_) %>%
     dplyr::mutate("_population_temp" := wppdistro::get_population(.data[[iso3]], year = pop_year)) %>%
     dplyr::group_by(dplyr::across(dplyr::any_of(c(iso3, ind, scenario))))
 
   for (i in 1:length(contribution)) {
     df <- df %>%
-      dplyr::mutate(!!sym(contribution[i]) := dplyr::case_when(
-        !(.data[[ind]] %in% ind_ids) ~ .data[[contribution[i]]],
-        !(.data[[year]] %in% end_year) ~ .data[[contribution[i]]],
-        TRUE ~ .data[["_population_temp"]] * (.data[[transform_value[i]]] - .data[[transform_value[i]]][.data[[year]] == !!start_year]) / 100
-      ))
+      dplyr::mutate(
+        !!sym(contribution_pct[i]) := dplyr::case_when(
+          !(.data[[ind]] %in% ind_ids) ~ .data[[contribution_pct[i]]],
+          !(.data[[year]] %in% end_year) ~ .data[[contribution_pct[i]]],
+          TRUE ~ (.data[[transform_value[i]]] - .data[[transform_value[i]]][.data[[year]] == !!start_year])
+        ),
+        !!sym(contribution[i]) := dplyr::case_when(
+          !(.data[[ind]] %in% ind_ids) ~ .data[[contribution[i]]],
+          !(.data[[year]] %in% end_year) ~ .data[[contribution[i]]],
+          TRUE ~ .data[["_population_temp"]] * .data[[contribution_pct[i]]] / 100
+        ))
   }
 
   dplyr::select(df, -"_population_temp") %>%
