591e9a006df55052407b4290426bacb6a80d386f
ElliottMess <36757002+ElliottMess@users.noreply.github.com>
2021-07-27 17:41:07 +0200

    Slight modification of calculate_hpop to have a functioning loop

--- R/calculate_hpop_contributions.R
+++ R/calculate_hpop_contributions.R
@@ -13,6 +13,9 @@
 #'     values. Must be the same length as `transform_value`.
 #' @param contribution_pct Column name of column(s) to store contribution (percent)
 #'     values. Must be the same length as `transform_value`.
+#' @param contribution_pct_pop_total Column name of column(s) to store contribution
+#' (percent of total population of the country) values. Must be the same length
+#' as `transform_value`.
 #' @param scenario Column name of column with scenario identifiers. Useful for
 #'     calculating contributions on data in long format rather than wide format.
 #'
@@ -27,6 +30,7 @@ calculate_hpop_contributions <- function(df,
                                          transform_value = "transform_value",
                                          contribution = stringr::str_replace(transform_value, "transform_value", "contribution"),
                                          contribution_pct = paste0(contribution, "_percent"),
+                                         contribution_pct_pop_total = paste0(contribution, "_percent_pop_total"),
                                          scenario = NULL,
                                          ind_ids = billion_ind_codes("hpop")) {
   assert_columns(df, year, iso3, ind, population, transform_value, scenario)
@@ -36,26 +40,40 @@ calculate_hpop_contributions <- function(df,
   assert_years(start_year, end_year)
 
   # add columns if not already existing
-  df <- billionaiRe_add_columns(df, c(contribution, contribution_pct), NA_real_)
+  df <- billionaiRe_add_columns(df, c(contribution, contribution_pct,contribution_pct_pop_total), NA_real_)
+
+  pop_total <- df %>%
+    dplyr::ungroup() %>%
+    dplyr::select(.data[[iso3]]) %>%
+    dplyr::distinct() %>%
+    dplyr::mutate("total_pop" := wppdistro::get_population(.data[[iso3]], year = max(end_year)))
 
   # calculate relevant contributions for each contribution column
-  df <- dplyr::group_by(df, dplyr::across(dplyr::any_of(c(iso3, ind, scenario))))
+  df <- dplyr::group_by(df, dplyr::across(dplyr::any_of(c(iso3, ind, scenario)))) %>%
+    dplyr::left_join(pop_total, by = iso3)
 
-  for (i in length(contribution)) {
+
+  for (i in seq(contribution)) {
     df <- dplyr::mutate(df,
-                        !!sym(contribution_pct[i]) := ifelse(
+                        !!rlang::sym(contribution_pct[i]) := ifelse(
                           !(.data[[ind]] %in% ind_ids) | !(.data[[year]] %in% end_year),
                           .data[[contribution_pct[i]]],
                           (.data[[transform_value[i]]] - .data[[transform_value[i]]][.data[[year]] == !!start_year])
                         ),
-                        !!sym(contribution[i]) := ifelse(
+                        !!rlang::sym(contribution[i]) := ifelse(
                           !(.data[[ind]] %in% ind_ids) | !(.data[[year]] %in% end_year),
                           .data[[contribution[i]]],
                           .data[[population]] * .data[[contribution_pct[i]]] / 100
+                        ),
+                        !!rlang::sym(contribution_pct_pop_total[i]) := ifelse(
+                          !(.data[[ind]] %in% ind_ids) | !(.data[[year]] %in% end_year),
+                          .data[[contribution_pct_pop_total[i]]],
+                          .data[[contribution[i]]] /.data[["total_pop"]] * 100
                         )
-    )
+                      )
   }
 
   dplyr::ungroup(df)
+  dplyr::select(df, -"total_pop")
 }
 
