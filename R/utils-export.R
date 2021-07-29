87fcefad0fc9f345d9a20b21ddab4d0b1e39e872
ElliottMess <36757002+ElliottMess@users.noreply.github.com>
2021-07-29 14:37:48 +0200

    Adding two small utiles to handle HPOP WASH names

--- R/utils-export.R
+++ R/utils-export.R
@@ -12,3 +12,24 @@ vec2emptyDF <- function(vec){
 
   return(df)
 }
+
+#' Removes unused WASH indicator from indicator list
+#'
+#' `remove_unused_wash_ind` removes
+#'
+#' @param ind_list character vector containing the list of indicator to be treated
+#' @param correct_wash character vector with the exact indicators to keep for WASH
+remove_unused_wash_ind <- function(ind_list, correct_wash){
+  wash_ind <- unique(stringr::str_extract(ind_list, paste0("^",correct_wash,"$", collapse = "|")))
+  wash_ind <- wash_ind[!is.na(wash_ind)]
+  c(ind_list[!stringr::str_detect(ind_list, "water|sanitation")], wash_ind)
+}
+
+#' Convert WASH names to remove the urban/rural element
+#'
+#' @param ind_list character vector with the indicators to be changed
+convert_wash_name<-function(ind_list) {
+  ind_list <- stringr::str_replace(ind_list, "^water.*", "water")
+  ind_list <- stringr::str_replace(ind_list, "^hpop_sanitation.*", "hpop_sanitation")
+  return(ind)
+}
