bc2b091b456b901cd457222f2f3cbdecfccf0cf7
ElliottMess <36757002+ElliottMess@users.noreply.github.com>
2021-08-05 18:21:10 +0200

    Change hpop_inter for new format

--- R/write_hpop_inter.R
+++ R/write_hpop_inter.R
@@ -1,9 +1,12 @@
 #' Write Inter sheet for HPOP
-#' @inherit write_main_df
-#' @inherit write_hpop_timeseries_sheet
+#' @inherit write_baseline_projection_hpop_summary
+#' @inheritParams write_hpop_timeseries_sheet
+#' @inheritParams export_hpop_country_summary_xls
+#' @inheritParams style_header_hpop_summary_sheet
 #' @param data_sheet_name character Name of the data sheet
-#' @param start_col_data integer Start column of the data frame in the data sheet
-#' @param start_row_data integer Start row of the data frame in the data sheet
+#' @param summary_bounds named list with boxes bounds of the different data 'boxes'
+#' present in the summary sheet. Used to identify where data sits in Excel sheet
+#' dynamically.
 
 write_hpop_inter <- function(wb,
                              sheet_name,
@@ -13,17 +16,17 @@ write_hpop_inter <- function(wb,
                              end_year,
                              start_col,
                              start_row,
-                             start_col_data = 1,
-                             start_row_data = 9,
-                             transform_value){
+                             transform_value,
+                             summary_bounds){
 
     l_trans_values <- length(transform_value)
 
-    position_first_trans_value <- 2 + (l_trans_values*2) + 1 # 2 indicators columns + raw_values columns + 1 to be at start of columns
+    start_trans <- l_trans_values*2+1
 
     # Write indicators
     openxlsx::writeData(wb, sheet_name, startCol = start_col, startRow = start_row+1,
                         x = data.frame(ind_df[,"short_name"]), colNames = FALSE)
+    row_range <- (summary_bounds$baseline_proj['start_row']+3):summary_bounds$baseline_proj['end_row']
 
     # start_year formulae
     ## start_year header
@@ -31,9 +34,9 @@ write_hpop_inter <- function(wb,
                         startRow = start_row,
                         x = start_year)
 
-    letter_position_start <- openxlsx::int2col(position_first_trans_value)
+    letter_position_start <- openxlsx::int2col(summary_bounds$baseline_proj['start_col']+start_trans)
     openxlsx::writeFormula(wb, sheet_name,
-                           x = glue::glue('=IF({data_sheet_name}!{letter_position_start}{start_row_data:(start_row_data+nrow(ind_df))}<>"",{data_sheet_name}!{letter_position_start}{start_row_data:(start_row_data+nrow(ind_df))},#N/A)'),
+                           x = glue::glue('=IF({data_sheet_name}!{letter_position_start}{row_range}<>"",{data_sheet_name}!{letter_position_start}{row_range},#N/A)'),
                            startCol = start_col+2, startRow = start_row +1)
 
     # end_year formulae
@@ -42,17 +45,17 @@ write_hpop_inter <- function(wb,
                         startRow = start_row,
                         x = max(end_year))
 
-    letter_position_end <- openxlsx::int2col(position_first_trans_value+1)
+    letter_position_end <- openxlsx::int2col(summary_bounds$baseline_proj['start_col']+start_trans+1)
     openxlsx::writeFormula(wb, sheet_name,
-                           x = glue::glue('=IF({data_sheet_name}!{letter_position_end}{start_row_data:(start_row_data+nrow(ind_df))}<>"",{data_sheet_name}!{letter_position_end}{start_row_data:(start_row_data+nrow(ind_df))},#N/A)'),
+                           x = glue::glue('=IF({data_sheet_name}!{letter_position_end}{row_range}<>"",{data_sheet_name}!{letter_position_end}{row_range},#N/A)'),
                            startCol = start_col + 3, startRow = start_row +1)
 
     # latest available
-    position_start_latest_available <- 2 + (l_trans_values*2)*2 + 4 + 1 + l_trans_values + 1 + l_trans_values*2 + l_trans_values*1 + 1
+    position_start_latest_available <- summary_bounds$latest["start_col"]+l_trans_values
     letter_position_latest <- openxlsx::int2col(position_start_latest_available)
 
     openxlsx::writeFormula(wb, sheet_name,
-                           x = glue::glue('=IF({data_sheet_name}!{letter_position_latest}{start_row_data:(start_row_data+nrow(ind_df))}<>"",{data_sheet_name}!{letter_position_latest}{start_row_data:(start_row_data+nrow(ind_df))},#N/A)'),
+                           x = glue::glue('=IF({data_sheet_name}!{letter_position_latest}{row_range}<>"",{data_sheet_name}!{letter_position_latest}{row_range},#N/A)'),
                            startCol = start_col + 8, startRow = start_row +1)
 
     #latest year
@@ -60,7 +63,7 @@ write_hpop_inter <- function(wb,
     letter_position_latest_year <- openxlsx::int2col(position_start_latest_year)
 
     openxlsx::writeFormula(wb, sheet_name,
-                           x = glue::glue('=IF({data_sheet_name}!{letter_position_latest_year}{start_row_data:(start_row_data+nrow(ind_df))}<>"",{data_sheet_name}!{letter_position_latest_year}{start_row_data:(start_row_data+nrow(ind_df))},#N/A)'),
+                           x = glue::glue('=IF({data_sheet_name}!{letter_position_latest_year}{row_range}<>"",{data_sheet_name}!{letter_position_latest_year}{row_range},#N/A)'),
                            startCol = start_col + 9, startRow = start_row +1)
 
     # Clear content below last row
