93ca6799e84f5767f9e5c30187fd12d4ee711bbc
ElliottMess <36757002+ElliottMess@users.noreply.github.com>
2021-08-09 11:35:26 +0100

    Adds two utils: get_baseline_projection_df and  get_latest_reported
    Adds two utils: get_baseline_projection_df and  get_latest_reported. Those functions get the data frames with respectively the baseline/projection data and the latest reported/estimated data that is being used in the export functions.

--- R/utils-summary.R
+++ R/utils-summary.R
@@ -31,3 +31,44 @@ get_ind_order <- function(ind){
     dplyr::left_join(billionaiRe::indicator_df, by = c(ind = "ind")) %>%
     dplyr::pull("order")
 }
+
+#' Get latest reported data frame
+#'
+#' `get_latest_reported()` gets the latest reported data available for estimated
+#' or reported data. Used in write functions.
+#'
+#' @inherit export_hpop_country_summary_xls
+get_latest_reported_df <- function(df, iso3, ind, type_col, year, value, transform_value = NULL, source_col){
+  df %>%
+    dplyr::filter(.data[[type_col]] %in% c("estimated", "reported")) %>%
+    dplyr::group_by(.data[[iso3]], .data[[ind]]) %>%
+    dplyr::filter(.data[[year]] == max(.data[[year]])) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(dplyr::all_of(c(ind, value,transform_value, year,
+                                  type_col, source_col))) %>%
+    dplyr::mutate(!!sym(year) := as.integer(.data[[year]]))
+}
+
+#' Get baseline and projections data frame for specified dates
+#'
+#' `get_latest_reported()` gets the latest reported data available for estimated
+#' or reported data. Used in write functions.
+#'
+#' @inherit export_hpop_country_summary_xls
+
+get_baseline_projection_df <- function(df, iso3, ind, type_col, year, value, transform_value,start_year, end_year, source_col){
+  df %>%
+    dplyr::filter(.data[[year]] %in% c(!!start_year, max(!!end_year))) %>%
+    dplyr::select(dplyr::all_of(c(ind, year, value, transform_value, type_col,
+                                  source_col, iso3))) %>%
+    dplyr::group_by(!!rlang::sym(ind), !!rlang::sym(iso3)) %>%
+    tidyr::pivot_wider(
+      names_from = !!rlang::sym(year),
+      values_from = c(dplyr::all_of(c(value, transform_value)), .data[[type_col]], .data[[source_col]])
+    ) %>%
+    dplyr::mutate(empty1 = NA, .after = glue::glue("{value}_{max(end_year)}")) %>%
+    dplyr::mutate(empty2 = NA, .after = glue::glue("{transform_value}_{max(end_year)}")) %>%
+    dplyr::mutate(empty3 = NA, .after = glue::glue("{type_col}_{max(end_year)}")) %>%
+    dplyr::ungroup()
+
+}
