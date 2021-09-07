b8e50bd00791885d77b8cc5e4fe10382bb53e443
Muhammad Jarir Kanji <mjkanji@gatech.edu>
2021-09-07 11:36:30 +0200

    save_wrangled_output now orders by ind, iso3, year (was just iso3, year before).

--- R/utils_wrangling.R
+++ R/utils_wrangling.R
@@ -112,7 +112,7 @@ save_wrangled_output <- function(df, path) {
     output_df <- df %>%
       dplyr::filter(whoville::is_who_member(.data[["iso3"]])) %>%
       dplyr::select(xmart_cols()) %>%
-      dplyr::arrange(.data[["iso3"]], .data[["year"]]) %>%
+      dplyr::arrange(.data[["ind"]], .data[["iso3"]], .data[["year"]]) %>%
       readr::write_csv(path, na = "")
 
     message("Output saved to disk.")
