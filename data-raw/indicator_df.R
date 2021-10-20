204c1763d2557191ffb92c00454bfcc284fc5757
ElliottMess <36757002+ElliottMess@users.noreply.github.com>
2021-10-20 15:48:24 +0300

    Clean indicator_df and merge with indicator_order (#26)
    This PR updates indicator_df. It:
    
    * combines indicator_df.xlsx and indicator_order.xlsx into one combined file.
    * updates the dashboard_id column to better align with the indicator names used in the Triple Billions dashboard backend.
    * updates definitions of eSPAR sub-indicators, 
    * corrects calculated values for hepi and prevent
    
    Co-authored-by: Muhammad Jarir Kanji <kanjim@who.int>

--- data-raw/indicator_df.R
+++ data-raw/indicator_df.R
@@ -2,16 +2,7 @@
 library(readxl)
 library(tidyverse)
 
-# Limited changes made, eventually going to live in xMart4
-indicator_order <- read_excel("data-raw/indicator_order.xlsx")
-
-
 indicator_df <- read_excel("data-raw/indicator_df.xlsx") %>%
-  mutate(across(
-    c(uhc, hpop, hep, covariate, calculated),
-    ~ ifelse(is.na(.x), F, T)
-  )) %>%
-  left_join(read_excel("data-raw/indicator_order.xlsx"), by = c("analysis_code" = "ind"), keep = TRUE) %>%
-  arrange(order)
+  arrange(hep, uhc, hpop, analysis_code)
 
 usethis::use_data(indicator_df, overwrite = TRUE, internal = FALSE)
