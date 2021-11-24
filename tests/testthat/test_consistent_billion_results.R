90dfd237c30f84f504806cd26bcabf128da559f4
Elliott <$GITHUB_ACTOR@users.noreply.github.com>
2021-11-24 16:30:24 +0200

    initiate unit testing for recycling and complexe calculations

--- tests/testthat/test_consistent_billion_results.R
+++ tests/testthat/test_consistent_billion_results.R
@@ -1,14 +1,13 @@
 
-# test_data <- billionaiRe::load_billion_data("all", "test_data")
-
-testthat::test_that("billion calculations are consistent",{
-
+testthat::test_that("basic billion calculations are consistent", {
   uhc_basic_calculated <- uhc_df %>%
     transform_uhc_data() %>%
     calculate_uhc_billion() %>%
     calculate_uhc_contribution(end_year = 2023, pop_year = 2023) %>%
-    dplyr::filter(ind %in% c("uhc_sm", "asc", "fh"),
-                  year == 2023)
+    dplyr::filter(
+      ind %in% c("uhc_sm", "asc", "fh"),
+      year == 2023
+    )
 
   hpop_basic_calculated <- hpop_df %>%
     transform_hpop_data() %>%
@@ -19,16 +18,23 @@ testthat::test_that("billion calculations are consistent",{
     transform_hep_data(extrapolate_to = 2023) %>%
     calculate_hep_components() %>%
     calculate_hep_billion(end_year = 2023, pop_year = 2023) %>%
-    dplyr::filter(ind %in% c("prevent",
-                             "espar",
-                             "detect_respond",
-                             "hep_idx"),
-                  year == 2023)
+    dplyr::filter(
+      ind %in% c(
+        "prevent",
+        "espar",
+        "detect_respond",
+        "hep_idx"
+      ),
+      year == 2023
+    )
 
   all_basic_calculated <- uhc_basic_calculated %>%
     dplyr::bind_rows(hpop_basic_calculated) %>%
     dplyr::bind_rows(hep_basic_calculated)
 
   testthat::expect_equal(all_basic_calculated, billionaiRe:::basic_test_calculated)
+})
 
+testthat::test_that("complexe billion calculations with one scenario are consistent", {
+  # test_data <- billionaiRe::load_billion_data("all", "test_data")
 })
---
+++ tests/testthat/test_data_recycling.R
@@ -0,0 +1,3 @@
+testthat::test_that("data recycling returns valid data frames", {
+  # test_data <- billionaiRe::load_billion_data("all", "test_data")
+})
