63305b29f299aff5869e45cf726dba66fb3ea49d
Elliott <$GITHUB_ACTOR@users.noreply.github.com>
2021-11-24 12:19:29 +0200

    document recycling functions

--- R/recycle_data.R
+++ R/recycle_data.R
@@ -1,3 +1,39 @@
+#' Recycle data between scenarios
+#'
+#' `recycle_data` recycles data between the scenarios present in `df` to reduce
+#' size of tables stored.
+#'
+#' This function wraps around `recycle_data_scenario_single` for all the
+#' scenarios present in the `scenario` column. `recycle_data_scenario_single `
+#' reuses values present in the specified  scenarios in `default_scenario`,
+#' `scenario_reported_estimated`, and `scenario_tp` for the specified scenarios.
+#'
+#' To do so, it looks at:
+#'
+#' 1. values in `default_scenario` but not in the scenario specified
+#' 2. values in `scenario_reported_estimated` but not in the scenario specified
+#' or `scenario_tp`
+#' 3. values in `scenario_tp` but not in the scenario specified,
+#' `scenario_reported_estimated` or `scenario_tp`
+#'
+#' For more information see:
+#'
+#' \code{vignette("scenarios", package = "billionaiRe")}
+#'
+#' @param billion name of billion to recycle data for.
+#' @param type Column name of column with types
+#' @param default_scenario name of the default scenario.
+#' @param scenario_reported_estimated name of the reported/estimated scenario.
+#' @param scenario_tp name of the WHO technical programs projections/imputations scenario.
+#' @param include_projection Boolean to include or not projections in recycling
+#' @param recycle_campaigns Boolean to include or not campaigns in recycling
+#'
+#' @inherit transform_hpop_data
+#' @inheritParams calculate_uhc_billion
+#' @inheritParams calculate_hpop_billion
+#'
+#' @return Data frame in long format.
+#' @export
 recycle_data <- function(df,
                          billion = c("hep", "hpop", "uhc"),
                          iso3 = "iso3",
@@ -7,30 +43,23 @@ recycle_data <- function(df,
                          type = "type",
                          start_year = 2018,
                          end_year = 2025,
-                         scenario_col = "scenario",
+                         scenario = "scenario",
                          default_scenario = "pre_covid_bau",
                          scenario_reported_estimated = "none",
                          scenario_tp = "tp",
                          include_projection = TRUE,
                          recycle_campaigns = TRUE,
                          ind_ids = NULL) {
-  assert_columns(df, iso3, ind, value, year, scenario_col, type)
-  assert_unique_rows(df, ind, iso3, year, scenario = scenario_col, ind_ids)
-  assert_scenario_in_df(df, c(scenario_reported_estimated, scenario_tp), scenario_col)
+  assert_columns(df, iso3, ind, value, year, scenario, type)
+  assert_unique_rows(df, ind, iso3, year, scenario = scenario, ind_ids)
+  assert_scenario_in_df(df, c(scenario_reported_estimated, scenario_tp), scenario)
 
   billion <- rlang::arg_match(billion)
   if (is.null(ind_ids)) {
     ind_ids <- billion_ind_codes(billion)
   }
 
-  scenarios_recycle <- unique(df[[scenario_col]])[!unique(df[[scenario_col]]) %in% c(scenario_reported_estimated, scenario_tp)]
-  # scenarios_recycle <- unique(df[[scenario_col]])
-
-  df_reported_tp <- df %>%
-    dplyr::filter(
-      .data[[scenario_col]] %in% c(scenario_reported_estimated, scenario_tp),
-      .data[[ind]] %in% ind_ids
-    )
+  scenarios_recycle <- unique(df[[scenario]])
 
   purrr::map_dfr(
     scenarios_recycle,
@@ -45,6 +74,7 @@ recycle_data <- function(df,
       type = type,
       start_year = start_year,
       end_year = end_year,
+      scenario_col = scenario,
       default_scenario = default_scenario,
       scenario_reported_estimated = scenario_reported_estimated,
       scenario_tp = scenario_tp,
@@ -53,10 +83,40 @@ recycle_data <- function(df,
       ind_ids = ind_ids
     )
   )
-  # %>%
-  #   dplyr::bind_rows(df_reported_tp)
 }
-
+#' Recycle data between scenarios for a single scenario
+#'
+#' `recycle_data_scenario_single ` reuses values present in the specified
+#' scenarios in `default_scenario`, `scenario_reported_estimated`, and
+#' `scenario_tp` for the specified scenarios.
+#'
+#' To do so, it looks at:
+#'
+#' 1. values in `default_scenario` but not in the scenario specified
+#' 2. values in `scenario_reported_estimated` but not in the scenario specified
+#' or `scenario_tp`
+#' 3. values in `scenario_tp` but not in the scenario specified,
+#' `scenario_reported_estimated` or `scenario_tp`
+#'
+#' For more information see:
+#'
+#' \code{vignette("scenarios", package = "billionaiRe")}
+#'
+#' @param scenario name of scenario to recycle for.
+#' @param scenario_col Column name of column with scenario identifiers.
+#' @param billion name of billion to recycle data for.
+#' @param type Column name of column with types
+#' @param default_scenario name of the default scenario.
+#' @param scenario_reported_estimated name of the reported/estimated scenario.
+#' @param scenario_tp name of the WHO technical programs projections/imputations scenario.
+#' @param include_projection Boolean to include or not projections in recycling
+#' @param recycle_campaigns Boolean to include or not campaigns in recycling
+#'
+#' @inherit transform_hpop_data
+#' @inheritParams calculate_uhc_billion
+#' @inheritParams calculate_hpop_billion
+#'
+#' @return Data frame in long format.
 recycle_data_scenario_single <- function(df,
                                          scenario,
                                          billion = c("hep", "hpop", "uhc"),
