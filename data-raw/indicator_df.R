b82db1a38e7c7bc560e74eae5067a773db996c19
Vassily Trubetskoy <trubetskoyv@who.int>
2022-09-13 14:54:22 +0200

    Revert "Changed GHO code for blood pressure to "Prevalence of hypertension (defined as having systolic blood pressure ≥140 mmHg, diastolic blood pressure ≥90 mmHg, or taking medication for hypertension) among adults aged 30-79.""
    This reverts commit ec1a876732ae6d4db528d4c90e8b307469c22d61.
    
    The change to `indicator_df` was made via the excel file, and exported to the .csv. This is the incorrect workflow, as it introduced additional changes. The .csv files should be edited directly.

--- data-raw/indicator_df.R
+++ data-raw/indicator_df.R
@@ -1,7 +1,7 @@
 ## code to prepare `indicator_df` dataset goes here
 library(tidyverse)
 
-indicator_df <- readr::read_delim("data-raw/indicator_df.csv", delim = ";") %>%
+indicator_df <- readr::read_csv("data-raw/indicator_df.csv") %>%
   arrange(hep, uhc, hpop, ind)
 
 usethis::use_data(indicator_df, overwrite = TRUE, internal = FALSE)
