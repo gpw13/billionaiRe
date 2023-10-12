ff02bcec2d871f2b74e0eb1ede973f71fe963eec
Charlton Callender <charlton.callender@gmail.com>
2023-10-12 12:31:47 +0900

    fix `small_is_best` for prevent indicator

--- data-raw/indicator_df.R
+++ data-raw/indicator_df.R
@@ -3,6 +3,8 @@ library(tidyverse)
 
 indicator_df <- readr::read_csv("data-raw/indicator_df.csv",
                                 locale = locale()) %>%
+  # HOTFIX: prevent is missing small is best value, needed for scenarios
+  mutate(small_is_best = if_else(ind == "prevent", FALSE, small_is_best)) %>%
   arrange(hep, uhc, hpop, ind)
 
 usethis::use_data(indicator_df, overwrite = TRUE, internal = FALSE)
